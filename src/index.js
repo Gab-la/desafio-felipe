10e6ff7b4131e68e72899761fe755fd7a8ec8ff4
Gab-la <gabrielcorreaglbc@gmail.com>
2026-01-30 17:09:25 -0300

    desafio do felipe.dio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-let nome = "Gabriel"
-console.log("Oi"+ nome)
+let nivelHeroi = 999999
+let rankHeroi
+let nomeHeroi = "Gabriel do Js"
+
+if(nivelHeroi<=1000){
+ rankHeroi="Ferro"
+}else if(nivelHeroi<=2000){
+rankHeroi="Bronze"
+}else if(nivelHeroi<=5000){
+rankHeroi="Prata"
+}else if(nivelHeroi<=7000){
+rankHeroi = "Ouro"  
+}else if(nivelHeroi<=8000){
+rankHeroi="Platina"
+}else if(nivelHeroi<=9000){
+rankHeroi= "Ascendente"
+}else if(nivelHeroi<=10000){
+rankHeroi="Imortal"
+}else{ (nivelHeroi>=10001)
+rankHeroi= "Radiante"}
+
+console.log("O Herói de nome "+ nomeHeroi, ("Está no Rank:")+rankHeroi, ("com ")+nivelHeroi, ("pontos"))
+
+
+
+
+
+
+
+
