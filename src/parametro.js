55e893c22fba09a19f1bc07d367c070f3cae6a8d
Gab-la <gabrielcorreaglbc@gmail.com>
2026-02-03 17:23:38 -0300

    aula

---
+++ src/parametro.js
@@ -0,0 +1,9 @@
+torrar("pao integral")
+
+
+function torrar (pao ,nome = "cliente" ,valor = 19.90){
+    console.log(` foi feito com=${pao} `)
+    console.log(`pedido de =${nome}`)
+    console.log(`valor de=${valor}`)
+    
+}
